d5c33d2120b53aa3a17db94d396474113292ed8c
Endre Dåvøy <lolskap@live.no>
2021-03-06 16:12:45 +0100

    added single order for interface between elevator driver and statehandler

--- datatypes/datatypes.go
+++ datatypes/datatypes.go
@@ -1,11 +1,26 @@
 package datatypes
 
+//This belongs in a config file
+const (
+	FloorCount    int = 4
+	ElevatorCount     = 3
+	ButtonCount       = 3
+)
+
 type MoveDirectionType int
 
 const (
 	MovingDown    MoveDirectionType = -1
 	MovingStopped                   = 0
 	MovingUp                        = 1
+)
+
+type ButtonType int
+
+const (
+	BtnHallUp   ButtonType = 0
+	BtnHallDown            = 1
+	BtnCab                 = 2
 )
 
 type MachineStateType int
@@ -19,11 +34,10 @@ const (
 	Completed
 )
 
-const (
-	FloorCount    int = 4
-	ElevatorCount     = 3
-	ButtonCount       = 3
-)
+type OrderType struct {
+	Button ButtonType
+	Floor  int
+}
 
 //OrderMatrixType is the type for the order matrix
 type OrderMatrixType [ButtonCount][FloorCount]OrderStateType
