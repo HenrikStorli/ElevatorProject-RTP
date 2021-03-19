67c894a85176339fcd8dd69b05f4ed60a40b188c
Endre Dåvøy <lolskap@live.no>
2021-03-19 16:31:25 +0100

    fixed some syntax errors

--- scheduler/costfunction.go
+++ scheduler/costfunction.go
@@ -1,36 +1,35 @@
 package scheduler
 
-import{
+func TimeToIdle(elevator ElevatorStates) int {
+	var duration int = 0
 
+	switch elevator.state {
+	case Idle:
+		newDirection = chooseDirection(elevator)
+		if newDirection == MovementStop {
+			return duration
+		}
+	case Moving:
+		duration += timeToTravel() / 2
+		elevator.floor += elevator.Direction
+	case OpenDoorState:
+		duration -= DoorOpenTime / 2
+	}
+
+	for {
+		if ElevatorShouldStop(elevator) {
+			elevator = clearOrdersAtCurrentFloor(elevator, NULL) // NULL means that the orders shouldnt really be cleared.
+			duration += DoorOpenTime
+			elevator.Direction = chooseDirection(elevator)
+			if elevator.Direction == MovementStop {
+				return duration
+			}
+		}
+		e.floor += elevator.Direction
+		duration += timeToTravel()
+	}
 }
 
-func TimeToIdle(elevator ElevatorStates) int {
-  var duartion int = 0
-
-  switch(elevator.state){
-  case Idle:
-    newDirection = chooseDirection(elevator)
-    if newDirection == MovementStop {
-      return duration
-      }
-  case Moving:
-    duration += TimeToTravel/2
-    elevator.floor += elevator.Direction
-  case OpenDoorState.
-    duartion -= DoorOpenTime/2
-  }
-
-
-  for {
-    if ElevatorShouldStop(elevator) {
-      elevator = clearOrdersAtCurrentFloor(elevator, NULL) // NULL means that the orders shouldnt really be cleared.
-      duartion += DoorOpenTime
-      elevator.Direction = chooseDirection(elevator)
-      if elevator.Direction == MovementStop {
-        return duration
-      }
-    }
-    e.floor += elevator.Direction
-    duration += TimeToTravel
-  }
+func timeToTravel() int {
+	return 0
 }
--- scheduler/runscheduler.go
+++ scheduler/runscheduler.go
@@ -1,67 +1,63 @@
 package scheduler
 
-import(
-  "fmt"
-  dt "../datatypes"
+import (
+	dt "../datatypes"
 )
 
 //Local variables needed in the moduel
 
+func RunOrdersScheduler(
+	newOrderIOCh <-chan dt.OrderType,
+	newOrderSHCh <-chan dt.OrderType,
+	elevatorStatesCh <-chan [dt.ElevatorCount]dt.ElevatorState,
+	orderMatricesCh <-chan [dt.ElevatorCount]dt.OrderMatrixType,
+	updateOrderMatricesCh chan<- [dt.ElevatorCount]dt.OrderMatrixType,
 
-func runOrdersScheduler(newOrderIOCh <-chan OrderType,
-                        newOrderSHCh <-chan OrderType,
-                        ElevatorStatesCh <-chan [dt.ElevatorCount]dt.ElevatorState,
-                        orderMatricesCh <-chan [dt.ElevatorCount]dt.OrderMatrixType,
-                        updateOrderMatricesCh chan<- [dt.ElevatorCount]dt.OrderMatrixType
+) {
+	var elevatorStatesCopy [dt.ElevatorCount]dt.ElevatorState
+	var orderMatricesCopy [dt.ElevatorCount]dt.OrderMatrixType
 
-){
-        var elevatorStatesCopy [dt.ElevatorCount]dt.ElevatorState
-        var orderMatricesCopy[dt.ElevatorCount]dt.OrderMatrixType
+	for {
+		select {
+		case newOrder := <-newOrderIOCh:
+			updatedOrderMatrices := placeOrder(newOrder, elevatorStatesCopy, orderMatricesCopy)
+			updateOrderMatricesCh <- updatedOrderMatrices
 
-        elevatorStatesCopy <- elevatorStatesCh
-        orderMatricesCopy <- orderMatricesCh
+		case newOrder := <-newOrderSHCh:
+			updatedOrderMatrices := placeOrder(newOrder, elevatorStatesCopy, orderMatricesCopy)
+			updateOrderMatricesCh <- updatedOrderMatrices
 
-        for{
-                select{
-                case newOrder := <- newOrderIOCh:
-						updatedOrderMatrices:= placeOrder(newOrder, elevatorStatesCopy, orderMatricesCopy)
-						updateOrderMatricesCh <- updatedOrderMatrices
-
-				case newOrder := <- newOrderSHCh:
-						updatedOrderMatrices:= placeOrder(newOrder, elevatorStatesCopy, orderMatricesCopy)
-						updateOrderMatricesCh <- updatedOrderMatrices
-
-                case elevatorStatesCopy <- elevatorStatesCh:
-
-                case orderMatricesCopy <- orderMatricesCh:
-                }
-        }
-
+		case elevatorStatesUpdate := <-elevatorStatesCh:
+			elevatorStatesCopy = elevatorStatesUpdate
+		case orderMatricesUpdate := <-orderMatricesCh:
+			orderMatricesCopy = orderMatricesUpdate
+		}
+	}
 
 }
 
+func placeOrder(
+	newOrder dt.OrderType,
+	elevatorStates [dt.ElevatorCount]dt.ElevatorState,
+	orderMatrices [dt.ElevatorCount]dt.OrderMatrixType,
+) [dt.ElevatorCount]dt.OrderMatrixType {
 
-func placeOrder(newOrder dt.OrderType,
-				elevatorStates [dt.ElevatorCount]dt.ElevatorState,
-				orderMatrices[dt.ElevatorCount]dt.OrderMatrixType
-				) [dt.ElevatorCount]dt.OrderMatrixType
-{
-		var fastestElevatorIndex int = 0
-		var fastestExecutionTime int = 1000
+	var fastestElevatorIndex int = 0
+	var fastestExecutionTime int = 1000
 
-		for elevatorIndex, state := range elevatorStates{
-				if state.IsFunctioning {
-						executionTime: = TimeToIdle(state, orderMatrices[elevatorIndex] )
+	for elevatorIndex, state := range elevatorStates {
+		if state.IsFunctioning {
+			executionTime := TimeToIdle(state, orderMatrices[elevatorIndex])
 
-						if executionTime < fastestExecutionTime {
-								fastestExecutionTime = executionTime
-								fastestElevatorIndex = elevatorIndex
-						}
-				}
+			if executionTime < fastestExecutionTime {
+				fastestExecutionTime = executionTime
+				fastestElevatorIndex = elevatorIndex
+			}
 		}
+	}
 
-		var updatedOrderMatrices [dt.ElevatorCount]dt.OrderMatrixType = orderMatrices
-		updatedOrderMatrices[fastestElevatorIndex][newOrder.ButtonType][newOrder.floor] = dt.New
+	var updatedOrderMatrices [dt.ElevatorCount]dt.OrderMatrixType = orderMatrices
+	updatedOrderMatrices[fastestElevatorIndex][newOrder.ButtonType][newOrder.floor] = dt.New
 
-		return updatedOrderMatrices
+	return updatedOrderMatrices
 }
