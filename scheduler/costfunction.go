27c67f33c44e1cf17954fda45224d47c180d9e49
Henrik Storli <henrisst@stud.ntnu.no>
2021-03-25 12:12:34 +0100

    Fixed names in constfunction. Defined door open and travel time

--- scheduler/costfunction.go
+++ scheduler/costfunction.go
@@ -2,39 +2,42 @@ package scheduler
 
 import (
 	dt "../datatypes"
+	ed "../elevatordriver"
+)
+
+const(
+	TRAVEL_TIME		int = 4
+	DOOR_OPEN_TIME		= 3
 )
 
 func TimeToIdle(elevator dt.ElevatorState, ordermatrix dt.OrderMatrixType) int {
-	var duration int = 0
+		var duration int = 0
 
-	switch elevator.State {
-	case dt.Idle:
-		newDirection := chooseDirection(elevator)
-		if newDirection == dt.MovingStopped {
-			return duration
+		switch elevator.State {
+		case dt.Idle:
+				newDirection := ChooseDirection(elevator)
+				if newDirection == dt.MovingStopped {
+						return duration
+				}
+		case dt.Moving:
+				duration += TRAVEL_TIME / 2
+				elevator.Floor += elevator.Direction
+		case dt.DoorOpen:
+				duration -= DOOR_OPEN_TIME / 2
 		}
-	case dt.Moving:
-		duration += timeToTravel() / 2
-		elevator.Floor += int(elevator.MovingDirection)
-	case dt.DoorOpen:
-		duration -= DoorOpenTime / 2
-	}
 
-	for {
-		if ElevatorShouldStop(elevator) {
-			elevator = clearOrdersAtCurrentFloor(elevator, nil) // nil means that the orders shouldnt really be cleared.
-			duration += DoorOpenTime
-			elevator.MovingDirection = chooseDirection(elevator)
-			if elevator.MovingDirection == dt.MovingStopped {
-				return duration
+		for {
+			if ElevatorShouldStop(elevator) {
+					elevator = ClearOrdersAtCurrentFloor(elevator, nil) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary 
+					duration += DOOR_OPEN_TIME
+					elevator.Direction = ChooseDirection(elevator)
+					if elevator.Direction == dt.MovingStopped {
+							return duration
+					}
 			}
+			elevator.Floor += elevator.Direction
+			duration += TRAVEL_TIME
 		}
-		elevator.Floor += int(elevator.MovingDirection)
-		duration += timeToTravel()
-	}
-	return duration
+		return duration
 }
 
-func timeToTravel() int {
-	return 0
-}
