8dbb225c4f2d69d9661d5306569901c082351388
Endre Dåvøy <lolskap@live.no>
2021-03-26 09:07:29 +0100

    fixed some syntax errors in costfunction

--- scheduler/costfunction.go
+++ scheduler/costfunction.go
@@ -5,39 +5,38 @@ import (
 	ed "../elevatordriver"
 )
 
-const(
-	TRAVEL_TIME		int = 4
-	DOOR_OPEN_TIME		= 3
+const (
+	TRAVEL_TIME    int = 4
+	DOOR_OPEN_TIME     = 3
 )
 
 func TimeToIdle(elevator dt.ElevatorState, ordermatrix dt.OrderMatrixType) int {
-		var duration int = 0
+	var duration int = 0
 
-		switch elevator.State {
-		case dt.Idle:
-				newDirection := ChooseDirection(elevator)
-				if newDirection == dt.MovingStopped {
-						return duration
-				}
-		case dt.Moving:
-				duration += TRAVEL_TIME / 2
-				elevator.Floor += elevator.Direction
-		case dt.DoorOpen:
-				duration -= DOOR_OPEN_TIME / 2
+	switch elevator.State {
+	case dt.Idle:
+		newDirection := ed.ChooseDirection(elevator)
+		if newDirection == dt.MovingStopped {
+			return duration
 		}
+	case dt.Moving:
+		duration += TRAVEL_TIME / 2
+		elevator.Floor += int(elevator.MovingDirection)
+	case dt.DoorOpen:
+		duration -= DOOR_OPEN_TIME / 2
+	}
 
-		for {
-			if ElevatorShouldStop(elevator) {
-					elevator = ClearOrdersAtCurrentFloor(elevator, nil) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary 
-					duration += DOOR_OPEN_TIME
-					elevator.Direction = ChooseDirection(elevator)
-					if elevator.Direction == dt.MovingStopped {
-							return duration
-					}
+	for {
+		if ed.ElevatorShouldStop(elevator) {
+			elevator = ed.ClearOrdersAtCurrentFloor(elevator, nil) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary
+			duration += DOOR_OPEN_TIME
+			elevator.MovingDirection = ed.ChooseDirection(elevator)
+			if elevator.MovingDirection == dt.MovingStopped {
+				return duration
 			}
-			elevator.Floor += elevator.Direction
-			duration += TRAVEL_TIME
 		}
-		return duration
+		elevator.Floor += int(elevator.MovingDirection)
+		duration += TRAVEL_TIME
+	}
+	return duration
 }
-
