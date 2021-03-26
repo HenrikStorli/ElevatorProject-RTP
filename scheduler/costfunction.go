efba1d80eaa328d2475a3cbcb4d49f1fdb404fd2
Henrik Storli <henrisst@stud.ntnu.no>
2021-03-26 10:20:03 +0100

    Created matrix converter for orders

--- scheduler/costfunction.go
+++ scheduler/costfunction.go
@@ -11,11 +11,12 @@ const (
 )
 
 func TimeToIdle(elevator dt.ElevatorState, ordermatrix dt.OrderMatrixType) int {
-	var duration int = 0
+	duration := 0
+	boolOrderMatrix := convertOrderTypeToBool(orderMatrix)
 
 	switch elevator.State {
 	case dt.Idle:
-		newDirection := ed.ChooseDirection(elevator)
+		newDirection := ed.ChooseDirection(elevator, boolOrderMatrix)
 		if newDirection == dt.MovingStopped {
 			return duration
 		}
@@ -28,9 +29,9 @@ func TimeToIdle(elevator dt.ElevatorState, ordermatrix dt.OrderMatrixType) int {
 
 	for {
 		if ed.ElevatorShouldStop(elevator) {
-			elevator = ed.ClearOrdersAtCurrentFloor(elevator, nil) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary
+			boolOrderMatrix = ed.ClearOrdersAtCurrentFloor(elevator, boolOrderMatrix, nil) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary
 			duration += DOOR_OPEN_TIME
-			elevator.MovingDirection = ed.ChooseDirection(elevator)
+			elevator.MovingDirection = ed.ChooseDirection(elevator, boolOrderMatrix)
 			if elevator.MovingDirection == dt.MovingStopped {
 				return duration
 			}
@@ -39,4 +40,16 @@ func TimeToIdle(elevator dt.ElevatorState, ordermatrix dt.OrderMatrixType) int {
 		duration += TRAVEL_TIME
 	}
 	return duration
+}
+
+func convertOrderTypeToBool(orderMatrix dt.OrderMatrixType) ed.orderMatrixBool {
+		var boolMatrix orderMatrixBool
+		for floor := 0; floor < dt.FloorCount; floor ++ {
+				for btnType := 0; btnType < dt.ButtonCount; btnType++ {
+						if orderMatrix[floor][btnType] == dt.Accepted {
+								boolMatrix[floor][btnType] = ed.ACTIVE
+						}
+				}
+		}
+		return boolMatrix
 }
