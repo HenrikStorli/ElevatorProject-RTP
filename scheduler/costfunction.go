97296fc147652843dd4309cce4dec5944f68e61b
Endre Dåvøy <lolskap@live.no>
2021-03-27 17:50:05 +0100

    removed print functions

--- scheduler/costfunction.go
+++ scheduler/costfunction.go
@@ -3,7 +3,6 @@ package scheduler
 import (
 	dt "../datatypes"
 	ed "../elevatordriver"
-	"fmt"
 )
 
 const (
@@ -14,12 +13,12 @@ const (
 func TimeToIdle(elevator dt.ElevatorState, orderMatrix dt.OrderMatrixType) int {
 	duration := 0
 	boolOrderMatrix := convertOrderTypeToBool(orderMatrix)
-	fmt.Println("Inside TimeToIdle")
+	//fmt.Println("Inside TimeToIdle")
 	switch elevator.State {
 	case dt.Idle:
 		newDirection := ed.ChooseDirection(elevator, boolOrderMatrix)
 		if newDirection == dt.MovingStopped {
-			fmt.Println("TimeToIdle first retun value")
+			//fmt.Println("TimeToIdle first retun value")
 			return duration
 		}
 	case dt.Moving:
@@ -29,9 +28,9 @@ func TimeToIdle(elevator dt.ElevatorState, orderMatrix dt.OrderMatrixType) int {
 		duration -= DOOR_OPEN_TIME / 2
 	default:
 	}
-	fmt.Println("Before For loop in costfunc")
+	//fmt.Println("Before For loop in costfunc")
 	for {
-		fmt.Println("For loop in costfunc")
+		//fmt.Println("For loop in costfunc")
 		if ed.ElevatorShouldStop(elevator, boolOrderMatrix) {
 			boolOrderMatrix = ed.ClearOrdersAtCurrentFloor(elevator, boolOrderMatrix) // nil means that the orders shouldnt really be cleared. I don't think that i is really necessary
 			duration += DOOR_OPEN_TIME
@@ -41,20 +40,20 @@ func TimeToIdle(elevator dt.ElevatorState, orderMatrix dt.OrderMatrixType) int {
 			}
 		}
 		elevator.Floor += int(elevator.MovingDirection)
-		//fmt.Printf("Elevator floor is: %v ", elevator.Floor)
+		////fmt.Printf("Elevator floor is: %v ", elevator.Floor)
 		duration += TRAVEL_TIME
 	}
 	return duration
 }
 
 func convertOrderTypeToBool(orderMatrix dt.OrderMatrixType) ed.OrderMatrixBool {
-		var boolMatrix ed.OrderMatrixBool
-		for floor := 0; floor < dt.FloorCount; floor ++ {
-				for btnType := 0; btnType < dt.ButtonCount; btnType++ {
-						if orderMatrix[btnType][floor] == dt.Accepted {
-								boolMatrix[btnType][floor] = ed.ACTIVE
-						}
-				}
+	var boolMatrix ed.OrderMatrixBool
+	for floor := 0; floor < dt.FloorCount; floor++ {
+		for btnType := 0; btnType < dt.ButtonCount; btnType++ {
+			if orderMatrix[btnType][floor] == dt.Accepted {
+				boolMatrix[btnType][floor] = ed.ACTIVE
+			}
 		}
-		return boolMatrix
+	}
+	return boolMatrix
 }
