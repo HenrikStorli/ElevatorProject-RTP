626035e3cfaf7a41d1eb1df5347f8a0b1be55f64
Endre Dåvøy <lolskap@live.no>
2021-03-28 13:33:27 +0200

    cab calls are now scheduled only to own elevator

--- scheduler/schedulermodule_test.go
+++ scheduler/schedulermodule_test.go
@@ -9,44 +9,15 @@ import (
 	"../scheduler"
 )
 
-// func TestSchedulerModule(*testing.T) {
-// 	newOrderIOCh := make(chan dt.OrderType)
-// 	newOrderSHCh := make(chan dt.OrderType)
-// 	elevatorStatesCh := make(chan [dt.ElevatorCount]dt.ElevatorState)
-// 	orderMatricesCh := make(chan [dt.ElevatorCount]dt.OrderMatrixType)
-// 	updateOrderMatricesCh := make(chan [dt.ElevatorCount]dt.OrderMatrixType)
-
-// 	go scheduler.RunOrdersScheduler(newOrderIOCh, newOrderSHCh,
-// 		elevatorStatesCh, orderMatricesCh, updateOrderMatricesCh)
-
-// 	var mockStates [dt.ElevatorCount]dt.ElevatorState
-// 	var mockOrders [dt.ElevatorCount]dt.OrderMatrixType
-// 	mockStates[0].IsFunctioning = true
-// 	go func() {
-// 		elevatorStatesCh <- mockStates
-// 		orderMatricesCh <- mockOrders
-// 		time.Sleep(10 * time.Millisecond)
-// 		newOrderIOCh <- dt.OrderType{Button: dt.BtnHallUp, Floor: 1}
-// 		time.Sleep(10 * time.Millisecond)
-// 		newOrderSHCh <- dt.OrderType{Button: dt.BtnHallDown, Floor: 3}
-// 	}()
-
-// 	updateOrderMatrices := <-updateOrderMatricesCh
-// 	fmt.Println(updateOrderMatrices)
-
-// 	updateOrderMatrices = <-updateOrderMatricesCh
-// 	fmt.Println(updateOrderMatrices)
-
-// }
-
 func TestSchedulerModule(*testing.T) {
-	newOrderIOCh := make(chan dt.OrderType)
-	newOrderSHCh := make(chan dt.OrderType)
+	newOrderCh := make(chan dt.OrderType)
 	elevatorStatesCh := make(chan [dt.ElevatorCount]dt.ElevatorState)
 	orderMatricesCh := make(chan [dt.ElevatorCount]dt.OrderMatrixType)
 	updateOrderMatricesCh := make(chan [dt.ElevatorCount]dt.OrderMatrixType)
 
-	go scheduler.RunOrdersScheduler(newOrderIOCh, newOrderSHCh,
+	elevatorID := 1
+
+	go scheduler.RunOrdersScheduler(elevatorID, newOrderCh,
 		elevatorStatesCh, orderMatricesCh, updateOrderMatricesCh)
 	//Define input
 	var mockStates [dt.ElevatorCount]dt.ElevatorState
@@ -61,15 +32,12 @@ func TestSchedulerModule(*testing.T) {
 	mockStates[0].MovingDirection = dt.MovingUp
 	mockStates[0].Floor = 1
 
-
-
 	elevatorStatesCh <- mockStates
 	orderMatricesCh <- mockOrders
 	time.Sleep(10 * time.Millisecond)
-	newOrderIOCh <- dt.OrderType{Button: dt.BtnHallUp, Floor: 1}
+	newOrderCh <- dt.OrderType{Button: dt.BtnHallUp, Floor: 1}
 	time.Sleep(10 * time.Millisecond)
-	newOrderSHCh <- dt.OrderType{Button: dt.BtnHallDown, Floor: 3}
-
+	newOrderCh <- dt.OrderType{Button: dt.BtnHallDown, Floor: 3}
 
 	updateOrderMatrices := <-updateOrderMatricesCh
 	fmt.Println(updateOrderMatrices)
